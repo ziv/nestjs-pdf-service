13db3c8d82d11d58600b50a6aa6e052628d68a6d
Ziv <ziv.perry@gmail.com>
2020-06-15 09:33:42 +0300

    pdf service

---
+++ src/pdf/pdf.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePdf } from './pdf';
+import * as puppeteer from 'puppeteer';
+
+@Injectable()
+export class PdfService {
+
+  async create(pdf: CreatePdf) {
+    try {
+      const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+      const page = await browser.newPage();
+      await page.setContent(pdf.html);
+      const buffer = await page.pdf(pdf.options);
+      await browser.close();
+      return buffer;
+
+    } catch (e) {
+      throw new Error(`Error creating PDF file: ${e}`);
+    }
+  }
+}
